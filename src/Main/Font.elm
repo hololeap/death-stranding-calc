92ce76a214b219269c250e7ab7ef8ca6652c3458
hololeap <hololeap@gmail.com>
2021-02-15 00:58:34 -0700

    Add default font

---
+++ src/Main/Font.elm
@@ -0,0 +1,13 @@
+module Main.Font exposing (..)
+
+import Element exposing (Attribute)
+import Element.Font as Font
+import Palette.Colors as Colors
+import Palette.Font.Size as FontSize
+
+defaultFont : List (Attribute msg)
+defaultFont =
+    [ FontSize.medium
+    , Font.color Colors.lightGrey
+    , Font.family [Font.sansSerif]
+    ]
